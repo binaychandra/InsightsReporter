a50c572e1fa5958a626b8a03f4d241d9e9297857
binaychandra <33412198+binaychandra@users.noreply.github.com>
2023-10-13 16:10:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def index():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=8090)
     #app.run(debug=True)

3dda5c6267100c1050b740ccf671dfd390cbc709
binaychandra <33412198+binaychandra@users.noreply.github.com>
2023-10-14 19:57:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def index():
                            divisionwiseanalysis_ytdstr=divisionwiseanalysis_ytdstr)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8090)
     #app.run(debug=True)
